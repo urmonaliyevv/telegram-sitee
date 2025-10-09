806f7e4bd86bf75a1decb7b4798cd8654a903de2
urmonaliyevv <urmonaliyevv@gmail.com>
2025-10-09 22:33:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('profileBtn').addEventListener('click', function() {
+  // Telegram profil havolangni shu yerga yoz
+  window.open('https://t.me/Urmonalievv', '_blank');
+});
+
+document.getElementById('channelBtn').addEventListener('click', function() {
+  // Telegram kanal havolangni shu yerga yoz
+  window.open('https://t.me/urmonalievvv', '_blank');
+});
